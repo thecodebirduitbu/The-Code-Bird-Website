439eee5637ed1796e7ab4cfa299bcccdd604f5cc
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-06-06 21:51:22 +0530

    Hero section + Domain (code in progress)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
 import Navbar from './Component/Navbar/Navbar';
-// import StarsCanvas from './Component/Star/Star';
+import "./index.css"
+import Hero from "./components/Hero"
+import  About from "./components/About";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Navbar/>
-      {/* <StarsCanvas/> */}
-      </header>
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+    
+       <Navbar/>
+       <Hero/>
+       </div>
+      
+      <div className='relative z-0'>
+      <About />
+      
+
+      </div>
+      
+      
+    
     </div>
+    </BrowserRouter>
   );
 }
 
