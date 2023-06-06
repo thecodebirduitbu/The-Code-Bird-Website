deb43caee8440a62393175052d9840ef75a76024
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-06-06 14:24:42 +0530

    codebird website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Component/Navbar/Navbar';
+// import StarsCanvas from './Component/Star/Star';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Navbar/>
+      {/* <StarsCanvas/> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
