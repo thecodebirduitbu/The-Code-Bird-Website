eac97d4ac82a48a1356e1bc48739b5f5ba3aed41
ImKKingshuk <hello@kingshukmondal.com>
2023-07-19 01:43:16 +0530

    Added New Testimonial.jsx , removed all junk CSS, now its fully responsive

---
+++ src/components/TestimonialsSlider/TestimonialsSlider.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+import img1 from "./img/profile-pic.png";
+
+function TestimonialsSlider() {
+  return (
+    <div>
+      <div className="my-8 ">
+        <div className="container flex flex-col items-center mx-auto mb-12 md:p-10 md:px-12">
+          <h1 className="p-4 text-4xl font-semibold leading-none text-center">
+            Don't just take our words... Read reviews from our members
+          </h1>
+        </div>
+        <div className="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
+          <div className="flex flex-col max-w-sm  mx-4 my-6 shadow-lg bg-gray-300 rounded-[22px]">
+            <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+              <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                  <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                </svg>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Voluptatibus quibusdam, eligendi exercitationem molestias
+                possimus facere.
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute right-0 w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                  <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                </svg>
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
+              <img
+                src={img1}
+                alt=""
+                className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
+              />
+              <p className="text-xl font-semibold leading-tight">
+                @ImKKingshuk
+              </p>
+              <p className="text-sm uppercase">Dev</p>
+            </div>
+          </div>
+          <div className="flex flex-col max-w-sm  mx-4 my-6 shadow-lg bg-gray-300 rounded-[22px]">
+            <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+              <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                  <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                </svg>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Voluptatibus quibusdam, eligendi exercitationem molestias
+                possimus facere.
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute right-0 w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                  <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                </svg>
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
+              <img
+                src={img1}
+                alt=""
+                className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
+              />
+              <p className="text-xl font-semibold leading-tight">Anuraj</p>
+              <p className="text-sm uppercase">Dev</p>
+            </div>
+          </div>
+          <div className="flex flex-col max-w-sm  mx-4 my-6 shadow-lg bg-gray-300 rounded-[22px]">
+            <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+              <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                  <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                </svg>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Voluptatibus quibusdam, eligendi exercitationem molestias
+                possimus facere.
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute right-0 w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                  <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                </svg>
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
+              <img
+                src={img1}
+                alt=""
+                className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
+              />
+              <p className="text-xl font-semibold leading-tight">Aditya</p>
+              <p className="text-sm uppercase">Dev</p>
+            </div>
+          </div>
+          <div className="flex flex-col max-w-sm  mx-4 my-6 shadow-lg bg-gray-300 rounded-[22px]">
+            <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+              <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                  <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                </svg>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Voluptatibus quibusdam, eligendi exercitationem molestias
+                possimus facere.
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute right-0 w-8 h-8 dark:text-violet-400"
+                >
+                  <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                  <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                </svg>
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
+              <img
+                src={img1}
+                alt=""
+                className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
+              />
+              <p className="text-xl font-semibold leading-tight">Rajdeep</p>
+              <p className="text-sm uppercase">Dev</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestimonialsSlider;
