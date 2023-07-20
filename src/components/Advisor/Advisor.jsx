c1cc2bdbe1144105c4c1555ab4ff4a33ed702015
ImKKingshuk <hello@kingshukmondal.com>
2023-07-20 23:16:42 +0530

    updated advisor

--- src/components/Advisor/Advisor.jsx
+++ src/components/Advisor/Advisor.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Advisor() {
   return (
-    <div>
-      <h1>advisor section here</h1>
+    <div className="pt-[8rem]">
+      <h1 className="text-[2rem] text-center">advisor section here</h1>
     </div>
   );
 }
