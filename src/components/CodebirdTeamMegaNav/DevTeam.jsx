bb848ee93c95fa6a2e6cd2cd04b85b0e4303b9e6
ImKKingshuk <hello@kingshukmondal.com>
2023-07-20 15:37:01 +0530

    added devteam

--- src/components/CodebirdTeamMegaNav/DevTeam.jsx
+++ src/components/CodebirdTeamMegaNav/DevTeam.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function DevTeam() {
   return (
     <div>
-      <h1>hi</h1>
+      <h1 className="text-xl text-center text-white">
+        Developers of this Website
+      </h1>
     </div>
   );
 }
