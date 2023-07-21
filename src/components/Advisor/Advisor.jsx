2ff996cd622cbc274f4c3104f92676c5d71c2428
ImKKingshuk <hello@kingshukmondal.com>
2023-07-21 15:00:23 +0530

    Added data in advisor

--- src/components/Advisor/Advisor.jsx
+++ src/components/Advisor/Advisor.jsx
@@ -3,7 +3,29 @@ import React from "react";
 function Advisor() {
   return (
     <div className="pt-[8rem]">
-      <h1 className="text-[2rem] text-center">advisor section here</h1>
+      <h1 className="text-[2rem] text-center">Our Advisor</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <div className="p-4 pt-2">
+          <h1 className="text-justify mx-auto text-md">
+            &#60; "The Code Bird" is the biggest coding club of UIT, where
+            budding tech enthusiasts flock together to spread their wings and
+            explore the fascinating world of programming. With engaging
+            workshops, collaborative projects, and a supportive community, we
+            nurture creativity, problem-solving, and a passion for coding. Join
+            us and soar to new coding heights! &#47; &#62;
+          </h1>
+        </div>
+
+        <div className="mx-auto p-6">
+          <Image
+            src="/codebird.png"
+            alt="Image of Developer"
+            sizes="fill"
+            width={1000}
+            height={1000}
+          />
+        </div>
+      </div>
     </div>
   );
 }
