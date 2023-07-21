419593e3109e9fdf53b5e86b747922cb723178fc
ImKKingshuk <hello@kingshukmondal.com>
2023-07-21 15:07:35 +0530

    advisor SGP Sirs bio updated

--- src/components/Advisor/Advisor.jsx
+++ src/components/Advisor/Advisor.jsx
@@ -6,13 +6,21 @@ function Advisor() {
       <h1 className="text-[2rem] text-center">Our Advisor</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 pt-[2rem] ">
         <div className="mx-auto">
-          <h1 className="text-justify mx-auto text-md">
-            &#60; "The Code Bird" is the biggest coding club of UIT, where
-            budding tech enthusiasts flock together to spread their wings and
-            explore the fascinating world of programming. With engaging
-            workshops, collaborative projects, and a supportive community, we
-            nurture creativity, problem-solving, and a passion for coding. Join
-            us and soar to new coding heights! &#47; &#62;
+          <h1 className="text-justify mx-auto text-lg">
+            Meet Mr. Sumit Gupta, an exceptional Assistant Professor in the
+            Department of Computer Science & Engineering at UIT BU. As the
+            guiding force behind "The Code Bird" coding club since its
+            inception, Professor Gupta has been instrumental in nurturing
+            excellence among coders at UIT. Under his mentorship, The Code Bird
+            has reached remarkable milestones and organized prestigious events
+            like CodeWiz, Snackdown, CIS Hackathon, and more, enabling UITians
+            to participate and shine on global stages like ACM ICPC, Snackdown,
+            and Bengalathon. Thanks to Professor Gupta's leadership, The Code
+            Bird team aims to expand its horizons, exploring new opportunities
+            and organizing diverse technical events for all students at UIT. We
+            are forever grateful for his contributions in fostering a thriving
+            Coding Culture at our institution and eagerly anticipate achieving
+            many more milestones under his esteemed guidance.
           </h1>
         </div>
 
