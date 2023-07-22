8b33684dbce02ad9d9ad9157fe09e7d18681f484
ImKKingshuk <hello@kingshukmondal.com>
2023-07-23 00:18:15 +0530

    timelineUi ball bug fixed,

--- src/components/CodebirdTeamMegaNav/Timeline.jsx
+++ src/components/CodebirdTeamMegaNav/Timeline.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Timeline({ events }) {
   return (
-    <div className="mx-10 md:mx-[3rem] ">
+    <div className="mx-6 md:mx-[3rem] ">
       <div className="relative">
         <div
           className="border-l-2 border-gray-300 absolute h-full top-0"
@@ -11,7 +11,7 @@ function Timeline({ events }) {
         {events.map(function (event, index) {
           return (
             <div key={index} className="flex items-start mb-8">
-              <div className="w-6 h-6 bg-blue-500 rounded-full border-2 border-blue-300 z-10"></div>
+              <div className="w-3 h-3 bg-blue-500 rounded-full border-2 border-blue-300 z-10"></div>
               <div className="ml-4">
                 <p className="text-gray-800 font-semibold">{event.date}</p>
                 <p className="text-gray-600">{event.description}</p>
