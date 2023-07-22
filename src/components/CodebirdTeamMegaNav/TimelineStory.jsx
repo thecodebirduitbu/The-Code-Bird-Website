c66945362038c89a36d517a7dca3ae39cb421147
ImKKingshuk <hello@kingshukmondal.com>
2023-07-23 00:10:59 +0530

    Added TimelineUi in TimelineStory.jsx

--- src/components/CodebirdTeamMegaNav/TimelineStory.jsx
+++ src/components/CodebirdTeamMegaNav/TimelineStory.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import Timeline from "./Timeline";
+
+const eventsData = [
+  {
+    date: "23rd August, 2016",
+    description:
+      "The Code Bird founded under the guidance of Professor Sumit Gupta.",
+    image: "event1.jpg",
+  },
+  {
+    date: "September, 2016",
+    description: "First coding contest organized: CodeWiz.",
+    image: "event2.jpg",
+  },
+  //  more events here
+];
 
 function TimelineStory() {
   return (
     <div className=" text-center text-white">
-      <h1>Its Story Time</h1>
+      <h1 className="">Its Story Time</h1>
+      <Timeline events={eventsData} />
     </div>
   );
 }
