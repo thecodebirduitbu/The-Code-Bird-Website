fe5d860782d6fe9f3e2b52a71ed674ecea729ef0
ImKKingshuk <hello@kingshukmondal.com>
2023-07-23 10:52:26 +0530

    timeline updated, added

--- src/components/CodebirdTeamMegaNav/Timeline.jsx
+++ src/components/CodebirdTeamMegaNav/Timeline.jsx
@@ -1,30 +1,35 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Timeline({ events }) {
   return (
-    <div className="mx-6 md:mx-[3rem] ">
-      <div className="relative">
-        <div
-          className="border-l-2 border-gray-300 absolute h-full top-0"
-          style={{ left: "50%" }}
-        ></div>
-        {events.map(function (event, index) {
-          return (
-            <div key={index} className="flex items-start mb-8">
-              <div className="w-4 h-4 bg-blue-500 rounded-full outline outline-white z-10"></div>
-              <div className="ml-4">
-                <p className="text-gray-200 font-semibold">{event.date}</p>
-                <p className="text-gray-400">{event.description}</p>
-              </div>
-              <img
-                src={`/eventimg/${event.image}`}
-                alt={`Event ${index + 1}`}
-                className="ml-4 w-24 h-24 rounded-lg object-cover"
-              />
+    <div className="mx-6 md:mx-[3rem] relative">
+      <div
+        className="absolute h-full w-1 left-1/2 bg-gray-300"
+        style={{ transform: "translateX(-50%)" }}
+      ></div>
+      {events.map(function (event, index) {
+        return (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6, delay: 0.2 * index }}
+            className="flex items-start mb-8 px-4 py-2 bg-white shadow-md rounded-2xl"
+          >
+            <div className="w-4 h-4 bg-blue-500 rounded-full z-10"></div>
+            <div className="ml-4">
+              <p className="text-gray-800 font-semibold">{event.date}</p>
+              <p className="text-gray-600">{event.description}</p>
             </div>
-          );
-        })}
-      </div>
+            <img
+              src={`/eventimg/${event.image}`}
+              alt={`Event ${index + 1}`}
+              className="ml-4 w-24 h-24 rounded-lg object-cover"
+            />
+          </motion.div>
+        );
+      })}
     </div>
   );
 }
