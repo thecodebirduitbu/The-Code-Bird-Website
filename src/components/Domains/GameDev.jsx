0eabed740acf2af6e1c60b5f2cc93461e284d4f2
ImKKingshuk <hello@kingshukmondal.com>
2023-07-25 22:34:18 +0530

    Added GameDev.jsx

---
+++ src/components/Domains/GameDev.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { TbBrandFlutter, TbBrandKotlin, TbJetpack } from "react-icons/tb";
+function GameDev() {
+  return (
+    <div className="py-[10rem] mx-10 md:mx-[2rem] text-white">
+      <h1 className="text-[2rem] text-white text-center">Game Development</h1>
+      <p className="text-lg mb-6 pt-16 text-justify">xyz</p>
+
+      <h3 className="text-xl font-semibold mb-2">Tech Stacks</h3>
+      <ul className="list-disc pl-6 mb-6">
+        <li>
+          <TbBrandKotlin className="inline-block mr-2 text-blue-500" />
+          xyz
+        </li>
+        <li>
+          <TbBrandFlutter className="inline-block mr-2 text-blue-500" />
+          xyz
+        </li>
+        <li>
+          <TbJetpack className="inline-block mr-2 text-blue-500" />
+          xyz
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-semibold mb-2">Learning Resources</h3>
+      <p className="mb-6">
+        Here are some recommended resources to enhance your web development
+        skills:
+      </p>
+      {/* list of learning resources here */}
+      <h3 className="text-xl font-semibold mb-2">Events and Workshops</h3>
+      <p className="mb-6">
+        Join us in exciting web development events and workshops:
+      </p>
+      {/* list of events and workshops here */}
+
+      <h3 className="text-xl font-semibold mb-2">Featured Projects</h3>
+      <p className="mb-6">
+        Check out some amazing projects created by our members:
+      </p>
+      {/* list of featured projects here */}
+    </div>
+  );
+}
+
+export default GameDev;
