a8b13897a5fbb1f638f680a3084a37bffb234c87
Puskar-Roy <puskarroy600@gmail.com>
2023-07-28 23:46:50 +0530

    event & faq done

--- src/components/Events/Prevevent.jsx
+++ src/components/Events/Prevevent.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Prevevent.css";
-import axios from 'axios'
+import axios from "axios";
 import Carousel from "react-multi-carousel";
 // import "react-multi-carousel/lib/styles.css";
 import Image1 from "../../assets/Eventpics/event2.png";
 import Image2 from "../../assets/Eventpics/event3.png";
 import Image3 from "../../assets/Eventpics/event1.png";
-
 
 const prevData = [
   {
@@ -65,28 +64,16 @@ const prevData = [
   },
 ];
 
-
-
 const Prevevent = () => {
   return (
-<<<<<<< HEAD
-    <div className="cardCarousel">
+    <div className="cardCarousel2">
       {prevData.map((item, key) => {
         return (
-          <div className="cardcontainer" key={key}>
-            <div className="card">
-=======
-     <div className="cardCarousel2">
-     
-        {prevData.map((item, key) => {
-          return (
-            <div className="card1" key={key}>
->>>>>>> 4c22668a27d20db084c050cb51fb38dd8e73d7ad
-              <img src={item.imgUrl} alt="" />
-              <div className="card__content1">
-                <p className="card__title1">{item.name}</p>
-                <p className="card__description1">{item.description}</p>
-              </div>
+          <div className="card1" key={key}>
+            <img src={item.imgUrl} alt="" />
+            <div className="card__content1">
+              <p className="card__title1">{item.name}</p>
+              <p className="card__description1">{item.description}</p>
             </div>
           </div>
         );
