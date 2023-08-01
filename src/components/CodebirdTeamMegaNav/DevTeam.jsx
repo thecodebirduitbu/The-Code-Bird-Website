03049c26a1e4fb67b541e9670dd1a6ee55e78305
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-01 21:15:56 +0530

    Recoded Componnet

--- src/components/CodebirdTeamMegaNav/DevTeam.jsx
+++ src/components/CodebirdTeamMegaNav/DevTeam.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import OurTechTeam from "../Team/OurTechTeam";
-import '../Team/OurTechTeam.css'
+import "../Team/OurTechTeam.css";
+const teamMembersData = [
+  {
+    name: "John Doe",
+    position: "Software Engineer",
+    imgUrl: "john-doe-img-url.jpg",
+    github: "https://github.com/johndoe",
+    linkedin: "https://linkedin.com/in/johndoe",
+    // No Instagram or Facebook links for John Doe
+  },
+  {
+    name: "Jane Smith",
+    position: "Web Developer",
+    imgUrl: "jane-smith-img-url.jpg",
+    github: "https://github.com/janesmith",
+    linkedin: "https://linkedin.com/in/janesmith",
+    instagram: "https://instagram.com/janesmith",
+    facebook: "https://facebook.com/janesmith",
+  },
+  // Add more team members as needed
+];
+
+// In your parent component
+
 function DevTeam() {
   return (
     <div className="py-[10rem]">
       <h1 className="text-[3rem] text-center text-white">
-        Developers of{" "}
-        <span className="thisWev">
-          this Website
-        </span>
+        Developers of <span className="thisWev">this Website</span>
       </h1>
       <div className="pt-10">
-        <OurTechTeam />
+        <OurTechTeam teamMembers={teamMembersData} />
       </div>
     </div>
   );
