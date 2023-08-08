d3f03b52126b9d1f24c4f706c05a5150961bdc27
Puskar-Roy <puskarroy600@gmail.com>
2023-08-08 22:47:28 +0530

    font update

--- src/components/Domains/WebDev.jsx
+++ src/components/Domains/WebDev.jsx
@@ -1,10 +1,96 @@
-import React from "react";
+import React , {useCallback} from "react";
 import { FaReact, FaCss3, FaHtml5 } from "react-icons/fa";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+
 
 function WebDev() {
+
+   const particlesInit = useCallback(async (engine) => {
+     await loadFull(engine);
+   }, []);
+
+   const particlesLoaded = useCallback(async (container) => {
+     await console.log(container);
+   }, []);
+
   return (
     <div className="py-[10rem] mx-10 md:mx-[2rem] text-white">
-      <h1 className="text-[2rem] text-white text-center">Web Development</h1>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {},
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 100,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: false,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: true,
+              speed: 3.2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <h1 className="text-[2.7rem] text-white text-center">Web <span className="brand">Development</span></h1>
       <p className="text-lg mb-6 pt-16 text-justify">
         Welcome to the Web Development domain! This exciting field empowers you
         to craft visually stunning and interactive websites. Whether you're
