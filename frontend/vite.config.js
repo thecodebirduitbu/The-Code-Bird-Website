b17a2b84784e2c8d7de650da93166bcafd4e996c
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-08-12 15:53:01 +0530

    dist file added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
-import envCompatible from 'vite-plugin-env-compatible';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envPrefix: 'REACT_APP_',
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: 'build',
-  },
-  plugins: [
-    react(),
-    envCompatible(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
-  ],
+  plugins: [react()],
 })
