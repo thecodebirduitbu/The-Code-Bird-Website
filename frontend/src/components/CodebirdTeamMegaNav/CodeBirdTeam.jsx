c8b24b31f2285c29d8365feb4d358b6241acd208
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-15 00:54:34 +0530

    Recoded core team, its fully responsive

--- frontend/src/components/CodebirdTeamMegaNav/CodeBirdTeam.jsx
+++ frontend/src/components/CodebirdTeamMegaNav/CodeBirdTeam.jsx
@@ -26,12 +26,12 @@ const teamMembers = [
 
 function CodeBirdTeam() {
   return (
-    <div className="py-[8rem] mx-6">
+    <div className="py-[8rem]">
       <h2 className="text-[3rem] text-center text-white">
         Our <span className="text-blue-500">Core Team</span>
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-6 mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-6 mx-6">
         {teamMembers.map((item, index) => (
           <div
             className="outline outline-white rounded-lg w-[17rem] h-fit text-center"
