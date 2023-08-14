78254900fcca134efc39271d784ed8b1bd7d545a
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-14 23:37:45 +0530

    “updated”

--- frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
+++ frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
@@ -83,7 +83,7 @@ const membersData = [
   },
   {
     id: "developer 3",
-    name: " ",
+    name: "Ayush Kumar",
     role: " ",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     socialLinks: [
