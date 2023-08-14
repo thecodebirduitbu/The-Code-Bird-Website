c2e44df1abc63f68e49b24b40c56b2c509c3b2e2
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-14 21:04:53 +0530

    “updated”

--- frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
+++ frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { FaGithub, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
+const socialLinkData = [
+  { id: "github", icon: FaGithub, color: "blue-500" },
+  { id: "twitter", icon: FaTwitter, color: "green-500" },
+  { id: "linkedin", icon: FaLinkedin, color: "indigo-500" },
+  { id: "instagram", icon: FaInstagram, color: "pink-500" },
+];
+
 const membersData = [
   {
+    id: "developer1",
     name: "Developer 1",
     role: "Software Engineer",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     socialLinks: [
-      { href: "#", icon: FaGithub, color: "blue-500" },
-      { href: "#", icon: FaTwitter, color: "green-500" },
-      { href: "#", icon: FaLinkedin, color: "indigo-500" },
-      { href: "#", icon: FaInstagram, color: "pink-500" },
+      { ...socialLinkData[0], href: "https://github.com/developer1" },
+      { ...socialLinkData[1], href: "https://twitter.com/developer1" },
+      { ...socialLinkData[2], href: "https://linkedin.com/in/developer1" },
+      { ...socialLinkData[3], href: "https://instagram.com/developer1" },
     ],
   },
   // Add more members here
@@ -18,7 +26,12 @@ const membersData = [
 
 function SocialIcon({ href, icon: Icon, color }) {
   return (
-    <a href={href} className={`text-${color} hover:text-gray-600`}>
+    <a
+      href={href}
+      className={`text-${color} hover:text-gray-600`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <Icon size={24} />
     </a>
   );
@@ -31,7 +44,7 @@ function MemberCard({ name, role, bio, socialLinks }) {
         <img
           className="object-center object-cover w-full h-full"
           src="/demo.png"
-          alt="photo"
+          alt={`${name}'s photo`}
         />
       </div>
       <div className="w-full md:w-3/5 text-left p-6 md:p-4 space-y-2">
@@ -41,10 +54,10 @@ function MemberCard({ name, role, bio, socialLinks }) {
           {bio}
         </p>
         <div className="flex justify-between mx-6">
-          {socialLinks.map((link, index) => (
+          {socialLinks.map((link) => (
             <SocialIcon
-              key={index}
-              href={link.href}
+              key={link.id}
+              href={link.href || "#"}
               icon={link.icon}
               color={link.color}
             />
@@ -55,24 +68,17 @@ function MemberCard({ name, role, bio, socialLinks }) {
   );
 }
 
-function MemberList({ members }) {
+function Founders() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mx-auto">
-      {members.map((member, index) => (
-        <MemberCard key={index} {...member} />
-      ))}
+    <div className="py-20 mx-6">
+      <h1 className="text-3xl text-white text-center pb-10">Our Founders</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mx-auto">
+        {membersData.map((member) => (
+          <MemberCard key={member.id} {...member} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default function Founders() {
-  return (
-    <div className="py-[10rem] mx-6">
-      <h1 className="text-[3rem] text-white text-center pb-10 ">
-        Our Founders
-      </h1>
-
-      <MemberList members={membersData} />
-    </div>
-  );
-}
+export default Founders;
