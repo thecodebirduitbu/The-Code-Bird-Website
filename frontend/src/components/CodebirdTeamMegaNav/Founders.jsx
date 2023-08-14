14495c5c56940177ddcdb759f4e98621dbfc6e1f
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-14 20:48:09 +0530

    “updated”

--- frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
+++ frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
@@ -52,7 +52,7 @@ function MemberCard({ name, role, bio, socialLinks }) {
 
 function MemberList({ members }) {
   return (
-    <div className="grid">
+    <div className="grid grid-cols-1 md:grid-cols-2">
       {members.map((member, index) => (
         <MemberCard key={index} {...member} />
       ))}
