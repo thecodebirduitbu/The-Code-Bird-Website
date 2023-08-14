c27f6a59ffcc931221a8f295aec8aec621aba748
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-14 20:55:42 +0530

    “updated”

--- frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
+++ frontend/src/components/CodebirdTeamMegaNav/Founders.jsx
@@ -2,34 +2,12 @@ import React from "react";
 import { FaGithub, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const membersData = [
-  {
-    name: "Developer 1",
-    role: "Software Engineer",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    socialLinks: [
-      { href: "#", icon: FaGithub },
-      { href: "#", icon: FaTwitter },
-      { href: "#", icon: FaLinkedin },
-      { href: "#", icon: FaInstagram },
-    ],
-  },
-  {
-    name: "Developer 2",
-    role: "Software Engineer",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    socialLinks: [
-      { href: "#", icon: FaGithub },
-      { href: "#", icon: FaTwitter },
-      { href: "#", icon: FaLinkedin },
-      { href: "#", icon: FaInstagram },
-    ],
-  },
-  // Add more members here
+  // ... (your member data)
 ];
 
-function SocialIcon({ href, icon: Icon }) {
+function SocialIcon({ href, icon: Icon, color }) {
   return (
-    <a href={href} className="text-gray-500 hover:text-gray-600">
+    <a href={href} className={`text-${color} hover:text-gray-600`}>
       <Icon size={24} />
     </a>
   );
@@ -38,24 +16,16 @@ function SocialIcon({ href, icon: Icon }) {
 function MemberCard({ name, role, bio, socialLinks }) {
   return (
     <div className="w-full bg-gray-900 rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row mb-4">
-      <div className="w-full md:w-2/5 h-80">
-        <img
-          className="object-center object-cover w-full h-full"
-          src="/demo.png"
-          alt="photo"
-        />
-      </div>
-      <div className="w-full md:w-3/5 text-left p-6 md:p-4 space-y-2">
-        <p className="text-xl text-white font-bold">{name}</p>
-        <p className="text-base text-gray-400 font-normal">{role}</p>
-        <p className="text-base leading-relaxed text-gray-500 font-normal">
-          {bio}
-        </p>
-        <div className="flex justify-between mx-6">
-          {socialLinks.map((link, index) => (
-            <SocialIcon key={index} href={link.href} icon={link.icon} />
-          ))}
-        </div>
+      {/* ... (rest of your component) */}
+      <div className="flex justify-between mx-6">
+        {socialLinks.map((link, index) => (
+          <SocialIcon
+            key={index}
+            href={link.href}
+            icon={link.icon}
+            color={link.color}
+          />
+        ))}
       </div>
     </div>
   );
