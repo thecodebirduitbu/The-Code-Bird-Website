78eeb69e258a169aea62c2602e2c237174c0dd13
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-15 00:40:57 +0530

    Recoded core team

--- frontend/src/components/CodebirdTeamMegaNav/CodeBirdTeam.jsx
+++ frontend/src/components/CodebirdTeamMegaNav/CodeBirdTeam.jsx
@@ -32,7 +32,10 @@ function CodeBirdTeam() {
 
       <div className="flex flex-wrap justify-center gap-8">
         {teamMembers.map((item, index) => (
-          <div className="outline outline-white rounded-lg w-1/4" key={index}>
+          <div
+            className="outline outline-white rounded-lg w-1/4 text-center"
+            key={index}
+          >
             <div className="w-full h-40 bg-white rounded-t-lg overflow-hidden flex items-center justify-center">
               <img
                 src={item.imgUrl}
@@ -46,34 +49,34 @@ function CodeBirdTeam() {
               </h4>
               <p className="text-gray-400">{item.position}</p>
 
-              <div className="flex items-center justify-center mt-3 space-x-2">
+              <div className="flex items-center justify-center mt-3 space-x-2 ">
                 <a
                   href={item.social.github}
-                  className="text-white text-lg hover:bg-gray-700 p-1 rounded-full"
+                  className="text-white text-lg hover:bg-gray-700   rounded-full"
                 >
                   <FaGithub />
                 </a>
                 <a
                   href={item.social.linkedin}
-                  className="text-white text-lg hover:bg-blue-500 p-1 rounded-full"
+                  className="text-white text-lg hover:bg-blue-500   rounded-full"
                 >
                   <FaLinkedin />
                 </a>
                 <a
                   href={item.social.twitter}
-                  className="text-white text-lg hover:bg-blue-400 p-1 rounded-full"
+                  className="text-white text-lg hover:bg-blue-400   rounded-full"
                 >
                   <FaTwitter />
                 </a>
                 <a
                   href={item.social.facebook}
-                  className="text-white text-lg hover:bg-blue-800 p-1 rounded-full"
+                  className="text-white text-lg hover:bg-blue-800   rounded-full"
                 >
                   <FaFacebook />
                 </a>
                 <a
                   href={item.social.instagram}
-                  className="text-white text-lg hover:bg-pink-600 p-1 rounded-full"
+                  className="text-white text-lg hover:bg-pink-600   rounded-full"
                 >
                   <FaInstagram />
                 </a>
