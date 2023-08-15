c830706b8ea660bb3530f307cc8718fe7df64166
Puskar-Roy <puskarroy600@gmail.com>
2023-08-15 21:50:51 +0530

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ require('./database/connectDatabase')
 
 
 const corsOptions = {
-  origin: "http://localhost:5174",
+  origin: "http://localhost:5173",
   credentials: true,
 };
 app.use(cors(corsOptions));
