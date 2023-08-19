2f3e5296a534e7c12e86a8dd14565b666bc68bd0
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-19 18:46:04 +0530

    updated

--- frontend/src/components/Homepages/HomePages.jsx
+++ frontend/src/components/Homepages/HomePages.jsx
@@ -7,21 +7,18 @@ import BlogUI1 from "../Blog/BlogUI1";
 import Achivements from "../MyComponent/Achivements";
 import ConfettiComponent from "../Confetti/Confetti";
 
-
 export default function HomePages() {
-  
-  
   return (
     //Its the Homepage / Index page of this site
     <div>
-    <ConfettiComponent/>
+      <ConfettiComponent />
       <Hero />
       <StarsCanvas />
       <About />
       <Achivements />
       <TestimonialsSlider />
       <BlogUI1 />
-      
+      {/*  */}
     </div>
   );
 }
